5e6a04aab11bb7c8a4a4449a8cbf9297a6b207cd
JakeVanCleave <jake@jake>
2023-01-21 14:37:55 -0800

    commit tree 6cbeac011a8c80669c49b19ff517a84794d4174a

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const main = document.getElementsByTagName("main")[0];
+main.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Jake is the champion";
+document.body.append(newHeader);
